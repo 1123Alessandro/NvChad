88b612ac1fa068ad08c4be5ff9309a7eedb88515
1123Alessandro <alessandroandyrei@gmail.com>
2023-10-22 20:46:35 +0800

    Removed unnecessary Mason LSPs

--- lua/custom/configs/overrides.lua
+++ lua/custom/configs/overrides.lua
@@ -23,20 +23,20 @@ M.treesitter = {
 
 M.mason = {
   ensure_installed = {
-    -- lua stuff
-    "lua-language-server",
-    "stylua",
-
-    -- web dev stuff
-    "css-lsp",
-    "html-lsp",
-    "typescript-language-server",
-    "deno",
-    "prettier",
-
-    -- c/cpp stuff
-    "clangd",
-    "clang-format",
+    -- -- lua stuff
+    -- "lua-language-server",
+    -- "stylua",
+    --
+    -- -- web dev stuff
+    -- "css-lsp",
+    -- "html-lsp",
+    -- "typescript-language-server",
+    -- "deno",
+    -- "prettier",
+    --
+    -- -- c/cpp stuff
+    -- "clangd",
+    -- "clang-format",
 
     -- my stuff
     "texlab",
